a1576f09eaa9f380c04c0bb192f1417a3b50820c
email <vali.netunachinova@seznam.cz>
2020-11-05 12:54:39 +0100

    first commit

---
+++ 1.project/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+*/
+
+// alert("Hello World!");
+
+// let answer = confirm("Are you here?");
+//console.log(answer);
+
+//let answer = +prompt("Jste zletily?", "Ano");
+//console.log(typeof(answer));
+
+//console.log("arr" + "- object");
+
+let incr = 10,
+decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+
+let isChecked = true;
+isClose = true;
+
+console.log(isChecked || isClose);
