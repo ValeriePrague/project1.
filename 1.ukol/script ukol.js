fb80cfe18cd24ad3a6536f249822d7b4fa92699a
email <vali.netunachinova@seznam.cz>
2020-11-05 22:29:42 +0100

    added files

---
+++ 1.ukol/script ukol.js
@@ -0,0 +1,46 @@
+let money = +prompt("Ваш бюджет на месяц?", ""),
+time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+
+let appData = {
+   budget: money,
+   expenses: {},
+   optionalExpenses: {},
+   income: [],
+   timeData: time,
+   savings: false 
+};
+
+
+for (let i =0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+    
+    if (typeof(a) === "string" && typeof(a) !=null && typeof(b)!= null
+    && a != "" && b != "" && a.length < 50) {  
+    console.log("done");    
+    appData.expenses[a] = b;
+    } else {
+    console.log("bad result");
+    i--;
+    }
+        
+    
+
+};
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Kazdodenni rozpocet: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Minimalni castka na zivobyti")
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Prumerny prijem");
+  } else if (appData.moneyPerDay > 2000) {
+        console.log("Vysoky prijem");
+    } else {
+    console.log("Chyba");
+ 
+}
